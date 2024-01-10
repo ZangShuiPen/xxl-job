b6c81a2d8098f4f778eecf3d030216d117ad3650
zhangsp <82261770+ZangShuiPen@users.noreply.github.com>
2024-01-10 14:04:52 +0800

    clean project
    清除项目

--- xxl-job-admin/src/test/java/com/example/xxljobadmin/XxlJobAdminApplicationTests.java
+++ xxl-job-admin/src/test/java/com/example/xxljobadmin/XxlJobAdminApplicationTests.java
@@ -1,37 +1,16 @@
 package com.example.xxljobadmin;
 
-import com.example.xxljobadmin.quartz.DynamicSchedulerUtil;
-import com.example.xxljobadmin.quartz.JobModel;
-import com.example.xxljobadmin.service.JobDetailDemo;
+
 import org.junit.jupiter.api.Test;
-import org.quartz.Job;
-import org.quartz.SchedulerException;
+
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 @SpringBootTest
 class XxlJobAdminApplicationTests {
 
     @Test
-    void contextLoads() throws SchedulerException, InterruptedException {
-        String triggerKeyName = "test";
-        String cronExpression = "0/3 * * * * ? *";
-        Class<JobDetailDemo> jobDetailDemoClass = JobDetailDemo.class;
-        Map<String, Object> jobDataMap = new HashMap<>();
-        jobDataMap.put("id", "1");
-        jobDataMap.put("name", "lisi");
-        DynamicSchedulerUtil.addJob(triggerKeyName, cronExpression, jobDetailDemoClass, jobDataMap);
-        DynamicSchedulerUtil.getScheduler().start();
-        List<Map<String, Object>> jobList = DynamicSchedulerUtil.getJobList();
-        System.out.println("jobList:"+jobList);
-        Thread.sleep(30000);
-//        DynamicSchedulerUtil.rescheduleJob(triggerKeyName,"0/5 * * * * ? *");
-//        Thread.sleep(400000);
-       // DynamicSchedulerUtil.pauseJob(triggerKeyName);
-
+    void contextLoads()  {
 
     }
 
